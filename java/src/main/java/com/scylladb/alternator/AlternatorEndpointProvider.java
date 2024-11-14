7f1adff9be5ef502990d5135a280fe372d4c900e
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2024-11-14 15:04:39 -0400

    Java: have a future cache

--- java/src/main/java/com/scylladb/alternator/AlternatorEndpointProvider.java
+++ java/src/main/java/com/scylladb/alternator/AlternatorEndpointProvider.java
@@ -1,7 +1,9 @@
 package com.scylladb.alternator;
 
 import java.net.URI;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 import software.amazon.awssdk.endpoints.Endpoint;
 import software.amazon.awssdk.services.dynamodb.endpoints.DynamoDbEndpointParams;
@@ -11,19 +13,26 @@ import software.amazon.awssdk.services.dynamodb.endpoints.DynamoDbEndpointProvid
 // choose a different endpoint for each request. Here we implement an
 // AlternatorEndpointProvider, which maintains up-to-date knowledge of the
 // live nodes in Alternator data center (by holding a AlternatorLiveNodes
-// object), and choose a different node for each request. 
+// object), and choose a different node for each request.
 public class AlternatorEndpointProvider implements DynamoDbEndpointProvider {
-	AlternatorLiveNodes liveNodes;
+	private final AlternatorLiveNodes liveNodes;
+	private final Map<URI, CompletableFuture<Endpoint>> futureCache;
+
 	public AlternatorEndpointProvider(URI seedURI) {
+		futureCache = new ConcurrentHashMap<>();
 		liveNodes = AlternatorLiveNodes.create(seedURI);
 	}
 
 	@Override
 	public CompletableFuture<Endpoint> resolveEndpoint(DynamoDbEndpointParams endpointParams) {
-		Endpoint ret = Endpoint.builder().url(liveNodes.nextAsURI()).build();
-		CompletableFuture<Endpoint> f = new CompletableFuture<Endpoint>();
-		f.complete(ret);
-		return f;
+		URI uri = liveNodes.nextAsURI();
+		CompletableFuture<Endpoint> endpoint = futureCache.getOrDefault(uri, null);
+		if (endpoint != null) {
+			return endpoint;
+		}
+		endpoint = new CompletableFuture<>();
+		endpoint.complete(Endpoint.builder().url(uri).build());
+		futureCache.put(uri, endpoint);
+		return endpoint;
 	}
-
 }

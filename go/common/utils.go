481fdda8bf5f7f019b73a0e7ed5322bdf24b2a26
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-03-11 10:43:42 -0400

    golang: cleanup unused code

--- go/common/utils.go
+++ go/common/utils.go
@@ -1,22 +1,8 @@
 package common
 
 import (
-	"errors"
 	"net/http"
-	"time"
 )
-
-func PatchHTTPClientIdleConnectionTimeout(httpClient *http.Client, IdleConnectionTimeout time.Duration) error {
-	if httpClient == nil {
-		return errors.New("failed to patch http transport to set IdleConnectionTimeout: http client is nil")
-	}
-	httpTransport, ok := httpClient.Transport.(*http.Transport)
-	if !ok {
-		return errors.New("failed to patch http transport to set IdleConnectionTimeout: transport is not *http.Transport")
-	}
-	httpTransport.IdleConnTimeout = IdleConnectionTimeout
-	return nil
-}
 
 func DefaultHTTPTransport() *http.Transport {
 	transport := http.DefaultTransport.(*http.Transport).Clone()

b552efb9b5e6250ff213fb479148fb57304d48f6
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-03-20 07:28:07 -0400

    java: fallback when server does not know rack or datacenter
    If rack is unknown, fallback to no rack.
    If datacenter is unkown fallback to having no rack and no datacenter.

--- java/src/main/java/com/scylladb/alternator/AlternatorEndpointProvider.java
+++ java/src/main/java/com/scylladb/alternator/AlternatorEndpointProvider.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import software.amazon.awssdk.endpoints.Endpoint;
 import software.amazon.awssdk.services.dynamodb.endpoints.DynamoDbEndpointParams;
@@ -44,20 +43,7 @@ public class AlternatorEndpointProvider implements DynamoDbEndpointProvider {
    */
   public AlternatorEndpointProvider(URI seedURI, String datacenter, String rack) {
     futureCache = new ConcurrentHashMap<>();
-    liveNodes = new AlternatorLiveNodes(seedURI, datacenter, rack);
-    try {
-      liveNodes.validate();
-      liveNodes.checkIfRackAndDatacenterSetCorrectly();
-      if (!datacenter.isEmpty() || !rack.isEmpty()) {
-        if (!liveNodes.checkIfRackDatacenterFeatureIsSupported()) {
-          logger.log(
-              Level.SEVERE,
-              String.format("server %s does not support rack or datacenter filtering", seedURI));
-        }
-      }
-    } catch (AlternatorLiveNodes.ValidationError | AlternatorLiveNodes.FailedToCheck e) {
-      throw new RuntimeException(e);
-    }
+    liveNodes = AlternatorLiveNodes.pickSupportedDatacenterRack(seedURI, datacenter, rack);
     liveNodes.start();
   }
 
--- java/src/main/java/com/scylladb/alternator/AlternatorRequestHandler.java
+++ java/src/main/java/com/scylladb/alternator/AlternatorRequestHandler.java
@@ -3,7 +3,6 @@ package com.scylladb.alternator;
 import com.amazonaws.Request;
 import com.amazonaws.handlers.RequestHandler2;
 import java.net.URI;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /* AlternatorRequestHandler is RequestHandler2 implementation for AWS SDK
@@ -39,20 +38,7 @@ public class AlternatorRequestHandler extends RequestHandler2 {
    * @since 1.0.1
    */
   public AlternatorRequestHandler(URI seedURI, String datacenter, String rack) {
-    liveNodes = new AlternatorLiveNodes(seedURI, datacenter, rack);
-    try {
-      liveNodes.validate();
-      liveNodes.checkIfRackAndDatacenterSetCorrectly();
-      if (!datacenter.isEmpty() || !rack.isEmpty()) {
-        if (!liveNodes.checkIfRackDatacenterFeatureIsSupported()) {
-          logger.log(
-              Level.SEVERE,
-              String.format("server %s does not support rack or datacenter filtering", seedURI));
-        }
-      }
-    } catch (AlternatorLiveNodes.ValidationError | AlternatorLiveNodes.FailedToCheck e) {
-      throw new RuntimeException(e);
-    }
+    liveNodes = AlternatorLiveNodes.pickSupportedDatacenterRack(seedURI, datacenter, rack);
     liveNodes.start();
   }
 
